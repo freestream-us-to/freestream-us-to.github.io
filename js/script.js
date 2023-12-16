e6f020cb65be5ca45a5745d9f33860770284009f
freestream <99094491+freestream-us-to@users.noreply.github.com>
2023-12-16 18:08:17 +0100

    Delete js/script.js

--- js/script.js
+++
@@ -1,4 +0,0 @@
-
-
-    window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1920},"pages":[{"id":"page-search","displayName":"Search","link":{"linkType":"LinkTypePage","href":"#!page-search"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
