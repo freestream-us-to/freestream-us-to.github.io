f521f4da486ec5226b09d3dc5ef07ccaf9c9c879
freestream <99094491+freestream-us-to@users.noreply.github.com>
2023-12-16 20:10:34 +0100

    Add files via upload

--- js/play.js
+++ js/play.js
@@ -37,52 +37,38 @@ var name = "";
 if(type=="mega"){
     embedlink = "https://mega.nz/embed/" + src;
     downloadlink = "https://mega.nz/file/" + src;
-    useadblock = false;
 }
 else if(type=="st"){
     embedlink = src;
     downloadlink = src;
-    useadblock = true;
 }
 else if(type=="voe"){
     embedlink = "https://voe.sx/e/" + src;
     downloadlink = "https://voe.sx/" + src;
-    useadblock = true;
 }
 else if(type=="vup"){
     embedlink = "https://vupload.com/e/" + src;
     downloadlink = "https://vupload.com/v/" + src;
-    useadblock = true;
 }
 else if(type=="ds2play"){
     embedlink = "https://ds2play.com/e/" + src;
     downloadlink = "https://ds2play.com/d/" + src;
-    useadblock = true;
 }
 else if(type=="vidoza"){
     embedlink = "https://vidoza.net/embed-" + src + ".html";
     downloadlink = "https://vidoza.net/embed-" + src + ".html";
-    useadblock = true;
 }
 else if(type=="mfire"){
-    embedlink = src;
+    embedlink = "/assets/error.mp4";
     downloadlink = "https://www.mediafire.com/file/" + src;
-    useadblock = true;
 } 
-else if(type=="Empty"){
-    embedlink = src;
-    downloadlink = src;
-    useadblock = true;
-}
-else if(type=="undefined"){
-    embedlink = src;
-    downloadlink = src;
-    useadblock = true;
+else if(type=="Empty" || type=="undefined"){
+    embedlink = "/assets/error.mp4";
+    downloadlink = "";
 }
 else{
-    embedlink = src;
-    downloadlink = src;
-    useadblock = true;
+    embedlink = "/assets/error.mp4";
+    downloadlink = "";
 }
 
 
